a619927c8fcf494ec763d3072e39591f6c50c3dc
Rodwyn <rodwynmoreno@gmail.com>
2019-05-30 17:13:52 -0500

    Added dependencies.

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: [
+      require('autoprefixer')({
+        'browsers': ['> 3%', 'last 3 versions']
+      })
+    ]
+}
---
+++ webpack-config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: { main: './src/scripts/index.jsx'},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'eslint-loader',
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/'
+          }
+        }]
+      },
+      {
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [{
+          loader: 'file-loader'
+        }]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /dist/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'style.css'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new StyleLintPlugin({}),
+    new CopyWebpackPlugin([{
+      from: __dirname + '/src/images',
+      to: './images/'
+    }])
+  ]
+};
