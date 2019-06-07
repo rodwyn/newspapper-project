3eb5647cdaededbf172e1f320441c51345381e66
Rodwyn <rodwynmoreno@gmail.com>
2019-06-07 18:12:46 -0500

    Added collection vertical component.

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,5 +1,6 @@
 import AppBarMenu from './components/AppBarMenu.jsx';
 import BreakingAlert from './components/BreakingAlert.jsx';
+import CollectionVertical from './components/CollectionVertical.jsx';
 import MainMasthead from './components/MainMasthead.jsx';
 import React from 'react';
 
@@ -7,10 +8,15 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <AppBarMenu />
         <BreakingAlert />
+        <AppBarMenu />
         <MainMasthead />
-        <h1>Holi crayoli</h1>
+        <div className="container-gap">
+          <div className="col-9">holi</div>
+          <div className="col-3">
+            <CollectionVertical />
+          </div>
+        </div>
       </div>
     );
   }
