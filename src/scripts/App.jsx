4bc97b359cd4f67b47138f5d3bdd0210652ca283
Salvador Espinosa <li.salvador.espinosa@gmail.com>
2019-06-11 16:41:32 -0500

    NPP-1 Corrected typo

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,6 +1,6 @@
 import AppBarMenu from './components/AppBarMenu.jsx';
 import BreakingAlert from './components/BreakingAlert.jsx';
-import MainMashead from './components/MainMasthead.jsx';
+import MainMasthead from './components/MainMasthead.jsx';
 import React from 'react';
 
 class App extends React.Component {
@@ -9,7 +9,7 @@ class App extends React.Component {
       <div>
         <AppBarMenu />
         <BreakingAlert />
-        <MainMashead />
+        <MainMasthead />
         <h1>Holi crayoli</h1>
       </div>
     );
