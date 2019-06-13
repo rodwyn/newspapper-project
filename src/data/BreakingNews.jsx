a02350066406d1139af4975d3b320ea4d9dbdc5f
Rodwyn <rodwynmoreno@gmail.com>
2019-06-13 16:57:29 -0500

    Added news data.

---
+++ src/data/BreakingNews.jsx
@@ -0,0 +1,29 @@
+export const BreakingNews = [
+  {
+    content: `Private, all-girls Westover School to open independent
+              investigation into past sexual assault allegations`,
+    link: '#',
+    timestamp: '4m'
+  },
+  {
+    content: `Wandering seal back in Long Island Sound after swimming 100 miles
+              up Connecticut River`,
+    link: '#',
+    timestamp: '32m'
+  },
+  {
+    content: 'Inmate accused of altering prison records behind bars',
+    link: '#',
+    timestamp: '1h'
+  },
+  {
+    content: 'Manchester police say stalker caught in the act',
+    link: '#',
+    timestamp: '3h'
+  },
+  {
+    content: 'Police release name of woman who died in New London crash',
+    link: '#',
+    timestamp: '3h'
+  }
+];
